e9e51807a7802b40617b6de1b915d8496f11384b
Sabryna Alves <sabryna.alves@interfile.com.br>
2023-06-02 17:30:53 -0300

    Simular digitacao selenium

---
+++ TypeTextLikeAHuman/Extensions/SeleniumExtension.cs
@@ -0,0 +1,66 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+
+namespace TypeTextLikeAHuman.Extensions;
+
+public static class SeleniumExtension
+{
+    public static void TypeTextLikeAHuman(this IWebElement element, string texto, bool allowError = false)
+    {
+        var palavras = texto.Split(' ');
+        var ultimaPalavra = palavras.Count();
+        int index = 0;
+
+        Random rnd = new Random();
+
+        foreach (string palavra in palavras)
+        {
+            Thread.Sleep(rnd.Next(150, 400));
+
+            foreach (char letra in palavra)
+            {
+                element.SendKeys(letra.ToString());
+
+
+                //Se for par digita rapido se for impar mais devagar
+                if (rnd.Next(0, 10) % 2 == 0)
+                {
+                    Thread.Sleep(rnd.Next(100, 150));
+                }
+                else
+                {
+                    Thread.Sleep(rnd.Next(200, 300));
+                }
+
+
+                //Erra uma letra e depois apaga
+                if (rnd.Next(0, 100) % 9 == 0 && allowError)
+                {
+                    element.SendKeys(Convert.ToChar(rnd.Next(97, 122)).ToString());
+                    Thread.Sleep(rnd.Next(150, 300));
+                    element.SendKeys(Keys.Backspace);
+                }
+
+            }
+
+            //Insere o espaço que foi removido com o Split exceto para a ultima palavra
+            if (++index != ultimaPalavra)
+                element.SendKeys(" ");
+        }
+    }
+
+    public static IWebElement WaitElement(this IWebDriver driver, By by, int seconds = 10)
+    {
+        try
+        {
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(seconds));
+            var element = wait.Until(d => d.FindElement(by));
+            return element;
+        }
+        catch
+        {
+            return null;
+        }
+    }
+}
---
+++ TypeTextLikeAHuman/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using TypeTextLikeAHuman.Extensions;
+
+string texto = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros mi, " +
+    "dignissim a auctor in, sollicitudin at odio. Integer non neque maximus tellus sagittis feugiat. " +
+    "Proin condimentum pulvinar leo, id condimentum ligula vehicula imperdiet. Aliquam eu elit non " +
+    "lectus blandit luctus sed at ex. Phasellus tempus elit at suscipit viverra. Mauris imperdiet" +
+    " nulla nec vestibulum malesuada. Phasellus urna sem, commodo ac dolor et, tempor mollis felis." +
+    " Etiam sed ex non nisl eleifend dignissim. Maecenas mattis condimentum volutpat. Maecenas" +
+    " pharetra egestas ex et molestie. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices" +
+    " posuere cubilia curae; Curabitur suscipit, felis ut.";
+
+
+ChromeDriver driver = new ChromeDriver();
+
+driver.Navigate().GoToUrl("https://www.google.com/intl/pt-PT/inputtools/try/");
+
+
+driver.WaitElement(By.XPath("//*[@id='demobox']")).TypeTextLikeAHuman(texto);
